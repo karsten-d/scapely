d44e949e54f4686e4ef5ad78d277c7d43c58aa88
Karsten Deininger <karsten.deininger@gmail.com>
2017-05-01 15:07:13 +0200

    Add code climate badge

--- scapely/tests/test_mesh.py
+++ scapely/tests/test_mesh.py
@@ -4,8 +4,5 @@ from scapely.mesh import Vertex
 
 def test_vertex_init():
     vertex = Vertex(0.0, 1.0, 2.0)
-    assert vertex.x == 0.0
-    assert vertex.y == 1.0
-    assert vertex.z == 2.0
-    assert vertex.error is None
-    assert not vertex.used
+    assert vertex.x == 0.0 and vertex.y == 1.0 and vertex.z == 2.0 \
+        and vertex.error is None and not vertex.used
